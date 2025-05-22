9f8188840b27fe7a9a17048722481fb8614fe017
Sasha Vak <mak12121212@gmail.com>
2025-05-22 15:45:39 +0300

    add bicycle

--- App.jsx
+++ App.jsx
@@ -1,53 +1,53 @@
+import { useState } from 'react'
 import './App.css'
 import { useTodos } from './hooks/useTodo'
 
 function App() {
     const { todos, addTodo } = useTodos()
+    const [inputValue, setInputValue] = useState('')
 
     const handleSubmit = async (e) => {
         e.preventDefault()
-        const input = e.target.elements.todoInput
-        const text = input.value.trim()
+        const text = inputValue.trim()
         // addTodo(text)
         // input.value = ''
 
-        if(!text) return;
-
-        addTodo(text)
-
+        if (!text) return
         try {
-            await fetch('http://localhost:3000/todos', {
-                method: 'POST',
-                headers: { 'Content-Type': 'application/json;charset=utf-8' },
-                body: JSON.stringify({ text })
-            })
+            await addTodo(text)
+            setInputValue('')
         } catch (error) {
-            console.error('Ошибка добавления:', error);
-            
+            console.error('Ошибка при добавлении задачи:', error)
         }
-        input.value = ''
     }
 
-    
+    const normalizedTodos = todos.map((todo) => ({
+        id: todo.id,
+        text: todo.text || todo.newTodo?.text || todo.textTrim || 'Без текста',
+    }))
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
+        <div className="app-container">
+            <h1>Список задач</h1>
+            <form className="todo-form" onSubmit={handleSubmit}>
                 <input
+                    value={inputValue}
+                    className="todo-input"
                     name="todoInput"
                     type="text"
                     placeholder="Задачи на сегодня"
+                    onChange={(e) => setInputValue(e.target.value)}
                 />
-                <button 
-            
-                type="submit">Записать</button>
+                <button className="todo-submit-button" type="submit">
+                    Записать
+                </button>
             </form>
-            <div className="todo-box">
-                {todos.map(({ id, text }) => {
-                    ;<div key={id}> {text} </div>
-                })}
+            <div className="todo-list">
+                {normalizedTodos.map(({ id, text }) => (
+                    <div key={id}>{text}</div>
+                ))}
             </div>
-        </>
+        </div>
     )
 }
 
--- hooks/useTodo.jsx
+++ hooks/useTodo.jsx
@@ -9,17 +9,39 @@ export function useTodos() {
             .then((loadedTodos) => {
                 setTodos(loadedTodos)
             })
-    }, [])  // Получение задачи
+    }, []) // Получение задачи
 
-    
-    const addTodo = (text) => {
-        if (!text.trim() === '') return
+    const addTodo = async (text) => {
+        const textTrim = text.trim()
+        if (!textTrim) return
 
-        const newTodo = {
-            id: Date.now(),
-            text,
+        // const newTodo = {
+        //     id: Date.now(),
+        //     text: textTrim,
+        // }
+
+        // отправка на сервер
+        try {
+            const response = await fetch('http://localhost:3005/todos', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json;charset=utf-8' },
+                body: JSON.stringify({ textTrim }),
+            })
+
+            // ответ от сервера
+
+            if (!response.ok) {
+                throw new Error('Ошибка при добавлении задачи')
+            }
+
+            const data = await response.json()
+
+            console.log(' Добавлена задача ', data)
+
+            setTodos((todoList) => [...todoList, data])
+        } catch (error) {
+            console.error('Ошибка:', error)
         }
-        setTodos((todoList) => [...todoList, newTodo])
     }
     return {
         todos,
